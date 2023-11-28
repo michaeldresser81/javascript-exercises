5a6f03e157d61e633e9a3956ec183415ad667843
Mike Dresser <116307817+michaeldresser81@users.noreply.github.com>
2023-11-28 11:07:32 -0500

    Framework up and running

--- dom-manipulation/javascript.js
+++ dom-manipulation/javascript.js
@@ -1,1 +1,5 @@
-alert('Hello, world!');
+let container = document.querySelector('#container');
+let para = document.createElement('p');
+para.textContent = 'Hey, I\'m red';
+para.style.color = 'red';
+container.appendChild(para);
